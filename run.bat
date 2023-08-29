3fcdc52538e4e9d5c5832c37a461700abd06a568
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-08-29 11:16:47 +0200

    'cleaning'

--- run.bat
+++ run.bat
@@ -3,10 +3,6 @@ set "script_path=%~dp0"
 set "lua_folder=%script_path%lua"
 set "luac_path=luac.out"
 
-echo The location of this batch script is: %script_path%
-echo The Lua folder path is: %lua_folder%
-echo Running luac.out located in: %luac_path%
-
 "%lua_folder%\lua54.exe" "%luac_path%"
 
 pause
