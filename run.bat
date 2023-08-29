b0b1f139aa21a7b5b60c5ccd3ef9c1ca7fd44b3f
Abdool-Zaid <aztoorabally7447@gmail.com>
2023-08-29 11:14:55 +0200

    'functional'

--- run.bat
+++ run.bat
@@ -1,8 +1,12 @@
 @echo off
 set "script_path=%~dp0"
-set "lua=lua"
+set "lua_folder=%script_path%lua"
+set "luac_path=luac.out"
 
 echo The location of this batch script is: %script_path%
-echo The new folder path is: %script_path%%lua%
+echo The Lua folder path is: %lua_folder%
+echo Running luac.out located in: %luac_path%
+
+"%lua_folder%\lua54.exe" "%luac_path%"
 
 pause
